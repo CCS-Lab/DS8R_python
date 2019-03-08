6ff96b6278d7481c9ad3bf290a4f33c38d6da522
Jaeyeong Yang <jaeyeong.yang1125@gmail.com>
2019-03-08 22:00:59 +0900

    Do not use version.py

--- ds8r/__init__.py
+++ ds8r/__init__.py
@@ -1,6 +1,5 @@
 import os
 
 from .ds8r import DS8R
-from .version import version as __version__
 
 __all__ = ['DS8R']
--- setup.py
+++ setup.py
@@ -1,95 +1,17 @@
-import os
 import sys
-import subprocess
 from codecs import open as codecs_open
 from setuptools import setup, find_packages
 
+# Check if the version is higher than 3.5.*
 if sys.version_info[:2] < (3, 5):
     raise RuntimeError("Python version >= 3.5 required.")
 
+# Version information
 MAJOR = 0
 MINOR = 1
 MICRO = 0
 ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-
-
-def git_version():
-    """
-    Return the git revision as a string
-    """
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-
-    return GIT_REVISION
-
-
-def get_version_info():
-    # Adding the git rev number needs to be done inside write_version_py(),
-    # otherwise the import of ds8r.version messes up the build under Python 3.
-    FULLVERSION = VERSION
-    if os.path.exists('.git'):
-        GIT_REVISION = git_version()
-    elif os.path.exists('ds8r/version.py'):
-        # must be a source distribution, use existing version file
-        try:
-            from ds8r.version import git_revision as GIT_REVISION
-        except ImportError:
-            raise ImportError("Unable to import git_revision. Try removing "
-                              "ds8r/version.py and the build directory "
-                              "before building.")
-    else:
-        GIT_REVISION = "Unknown"
-
-    if not ISRELEASED:
-        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
-
-    return FULLVERSION, GIT_REVISION
-
-
-def write_version_py(filename='ds8r/version.py'):
-    cnt = """
-# THIS FILE IS GENERATED FROM DS8R SETUP.PY
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-release = %(isrelease)s
-if not release:
-    version = full_version
-"""
-    FULLVERSION, GIT_REVISION = get_version_info()
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % {'version': VERSION,
-                       'full_version': FULLVERSION,
-                       'git_revision': GIT_REVISION,
-                       'isrelease': str(ISRELEASED)})
-    finally:
-        a.close()
-
-
-write_version_py()
-
 
 # Get the long description from the relevant file
 with codecs_open('README.md', encoding='utf-8') as f:
