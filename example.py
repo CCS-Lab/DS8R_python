c27e2d96529c0290420599dfa2fbff71a135b2e6
Jihyun (Cindy) Hur <44596787+Jihyuncindyhur@users.noreply.github.com>
2021-01-12 15:03:25 +0900

    Make the variable name the same as in README.me

--- example.py
+++ example.py
@@ -2,23 +2,23 @@ import time
 
 from DS8R_class import DS8R
 
-level5 = DS8R(mode=1, polarity=1, source=1,demand=20,
+ctl5 = DS8R(mode=1, polarity=1, source=1,demand=20,
               pulse_width=1000, dwell=10, recovery=20, enabled=1)
-level4 = DS8R(demand=30, pulse_width=900)
-level3 = DS8R(demand=20, pulse_width=800)
-level2 = DS8R(demand=30, pulse_width=700)
-level1 = DS8R(demand=20, pulse_width=600)
+ctl4 = DS8R(demand=30, pulse_width=900)
+ctl3 = DS8R(demand=20, pulse_width=800)
+ctl2 = DS8R(demand=30, pulse_width=700)
+ctl1 = DS8R(demand=20, pulse_width=600)
 
-level5.run()
+ctl5.run()
 time.sleep(1)
 
-level4.run()
+ctl4.run()
 time.sleep(1)
 
-level3.run()
+ctl3.run()
 time.sleep(1)
 
-level2.run()
+ctl2.run()
 time.sleep(1)
 
-level1.run()
+ctl1.run()
