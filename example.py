5c82f5e31fb08dfa91d30ecb9d3b3fd3cee1cd0b
Jihyun (Cindy) Hur <44596787+Jihyuncindyhur@users.noreply.github.com>
2021-01-12 15:24:06 +0900

    Minor fix

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
 import time
 from DS8R_class import DS8R
 
-# create an object of the DS8R class and set parameter values.
+# create objects of the DS8R class and set parameter values.
 ctl1 = DS8R(mode=1, polarity=1, source=1,demand=20,
-              pulse_width=1000, dwell=10, recovery=20, enabled=1)
-ctl2 = DS8R(demand=30, pulse_width=900)
-ctl3 = DS8R(demand=20, pulse_width=800)
-ctl4 = DS8R(demand=30, pulse_width=700)
-ctl5 = DS8R(demand=20, pulse_width=600)
+              pulse_width=700, dwell=10, recovery=20, enabled=1)
+ctl2 = DS8R(demand=25, pulse_width=700)
+ctl3 = DS8R(demand=30, pulse_width=700)
+ctl4 = DS8R(demand=20, pulse_width=800)
+ctl5 = DS8R(demand=25, pulse_width=800)
+ctl6 = DS8R(demand=30, pulse_width=800)
 
 # apply parameters and trigger
 ctl1.run()
@@ -23,3 +24,6 @@ ctl4.run()
 time.sleep(1)
 
 ctl5.run()
+time.sleep(1)
+
+ctl6.run()
